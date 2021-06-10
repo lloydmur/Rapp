14aa5c344c4aa13848347c4b09d2a30c13a5a1b0
lloydmur <lloydmur4@gmail.com>
2021-06-10 19:17:46 -0400

    Initial upload

---
+++ src/components/newPost.js
@@ -0,0 +1,10 @@
+const newPost = () => {
+    return ( 
+        <div className="newPost">
+            <h2>Create a new Post!</h2>
+            
+        </div>
+     );
+}
+ 
+export default newPost;
